36a9794048fce2665e9072cceaba1b6c8b880309
freh-g <francesco.gualdi01@estudiant.upf.edu>
2023-09-15 18:49:30 +0200

    updated new version2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name='genopyc',
     packages=find_packages(),
-    version='2.0.7',
+    version='2.0.9',
     long_description=long_description,
     include_package_data=True,
     package_data={'genopyc': ['data/*']},
