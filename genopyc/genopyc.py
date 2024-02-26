a38541fcfd2de6fecf10126f5df2ed9405545441
freh-g <francesco.gualdi01@estudiant.upf.edu>
2024-02-26 16:44:00 +0100

    wrote code in PEP8 standards and added gene mapping functionalities

--- genopyc/genopyc.py
+++ genopyc/genopyc.py
@@ -23,6 +23,128 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input,Output
 import dash_cytoscape as cyto
+import pandas as pd
+import requests
+import os
+import re
+import wget
+
+
+
+ncbidb = pd.read_csv(os.path.join(location, 'data', 'Homo_sapiens.gene_info.gz'),sep='\t')
+tab_uni =  pd.read_csv(os.path.join(location, 'data', 'HUMAN_9606_idmapping.dat.gz'),sep='\t',names=['uniprot','mapper','id'])
+
+
+def update_mapping_datasets(mrconso_path):
+    
+    """
+    Updates mapping datasets required for gene mapping.
+
+    Downloads NCBI and UniProt mapping files, and creates UMLS dictionaries.
+
+    Args:
+        mrconso_path (str): Path to the MRCONSO file.
+
+    Raises:
+        FileNotFoundError: If the MRCONSO file is not found at the specified path.
+    """
+    
+    location = os.path.dirname(os.path.realpath(__file__))
+    out = os.path.join(location, 'datasets')
+    ncbidb='https://ftp.ncbi.nih.gov/gene/DATA/GENE_INFO/Mammalia/Homo_sapiens.gene_info.gz'
+    tab_uni='https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/idmapping/by_organism/HUMAN_9606_idmapping.dat.gz'	 
+    print('Downloading NCBI mapping file\n')
+    wget.download(ncbidb,out)
+    print('\nDownloading UniProt mapping file ')
+    wget.download(tab_uni,out)
+    print('\n Creating umls dictionaries ')
+    if os.path.isfile(mrcons_path):
+    
+    else:
+        raise FileNotFoundError(f'no such file or directory {mrconso_path}')
+    
+
+    print('\n!!DATABASES UPDATED!!')
+
+
+
+def gene_mapping_many(query_list,source,target):
+    
+    """
+    Maps gene identifiers from one namespace to another.
+
+    Args:
+        query_list (list): List of gene identifiers to be mapped.
+        source (str): Source namespace of the gene identifiers.
+        target (str): Target namespace to which the gene identifiers will be mapped.
+
+    Returns:
+        list: List of gene identifiers mapped to the target namespace.
+
+    Raises:
+        AssertionError: If input query_list contains non-integer values when source or target is 'entrez'.
+
+    Note:
+        The function relies on external data sources 'ncbidb' and 'tab_uni'.
+    """
+    
+    
+
+    if source=='ensembl' and target=='symbol':
+        ense=ncbidb['dbXrefs'].apply(lambda x : re.sub(r'.+?(?=ENS)', '', x))
+        dictio=dict((x.split('|')[0], y) for x, y in list(zip(ense,ncbidb['Symbol'])) if 'ENS' in x)
+        return list(map(dictio.get,query_list))
+
+    elif source=='ensembl' and target=='entrez':
+        ense=ncbidb['dbXrefs'].apply(lambda x : re.sub(r'.+?(?=ENS)', '', x))
+        dictio=dict((x.split('|')[0], y) for x, y in list(zip(ense,ncbidb['GeneID'])) if 'ENS' in x)
+        return list(map(dictio.get,query_list))
+
+    elif source=='ensembl' and target=='uniprot':
+        dictio=dict(zip(list(reversed(tab_uni[tab_uni.mapper=='Ensembl'].id.tolist())),
+                list(reversed(tab_uni[tab_uni.mapper=='Ensembl'].uniprot.tolist()))))
+        return list(map(dictio.get,query_list))
+
+    elif source=='entrez' and target=='ensembl':
+        assert all(isinstance(x, int) for x in query_list), "All entrez_ids in query list should be integers"
+        ense=ncbidb['dbXrefs'].apply(lambda x : re.sub(r'.+?(?=ENS)', '', x))
+        dictio=dict((y, x.split('|')[0]) for x, y in list(zip(ense,ncbidb['GeneID'])) if 'ENS' in x)
+        return list(map(dictio.get,query_list))
+
+    elif source == 'entrez' and target == 'symbol':
+        assert all(isinstance(x, int) for x in query_list), "All entrez_ids in query list should be integers"
+        dictio=dict((x, y) for x, y in list(zip(ncbidb['GeneID'],ncbidb['Symbol'])))
+        return list(map(dictio.get,query_list))
+
+    elif source=='entrez' and target=='uniprot':
+        assert all(isinstance(x, int) for x in query_list), "All entrez_ids in query list should be integers"
+        dictio=dict(zip([int(g) for g in list(reversed(tab_uni[tab_uni.mapper=='GeneID'].id.tolist()))],
+                list(reversed(tab_uni[tab_uni.mapper=='GeneID'].uniprot.tolist()))))
+        return list(map(dictio.get,query_list))
+
+    elif source=='symbol' and target=='ensembl':
+        ense=ncbidb['dbXrefs'].apply(lambda x : re.sub(r'.+?(?=ENS)', '', x))
+        dictio=dict((y, x.split('|')[0]) for x, y in list(zip(ense,ncbidb['Symbol'])) if 'ENS' in x)
+        return list(map(dictio.get,query_list))
+
+    elif source == 'symbol' and target == 'entrez':
+        dictio=dict((y, x) for x, y in list(zip(ncbidb['GeneID'],ncbidb['Symbol'])))
+        return list(map(dictio.get,query_list))
+
+    elif source=='symbol' and target=='uniprot':
+        dictio=dict(zip(list(reversed(tab_uni[tab_uni.mapper=='Gene_Name'].id.tolist())),
+                list(reversed(tab_uni[tab_uni.mapper=='Gene_Name'].uniprot.tolist()))))
+        return list(map(dictio.get,query_list))
+
+    elif source=='uniprot' and target=='ensembl':
+        dictio=dict(zip(list(reversed(tab_uni[tab_uni.mapper=='Ensembl'].uniprot.tolist())),
+                list(reversed(tab_uni[tab_uni.mapper=='Ensembl'].id.tolist()))))
+        return list(map(dictio.get,query_list))
+
+    elif source=='uniprot' and target=='entrez':
+        dictio=dict(zip(list(reversed(tab_uni[tab_uni.mapper=='GeneID'].uniprot.tolist())),
+                [int(g) for g in list(reversed(tab_uni[tab_uni.mapper=='GeneID'].id.tolist()))]))
+        return list(map(dictio.get,query_list))
 
 
 def get_associations(efotrait, verbose=False, studyid=False):
